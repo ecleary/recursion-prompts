a496486ca62503325a8e4165e123696c716039e0
Errol Cleary <errol.cleary@gmail.com>
2020-10-29 14:20:37 -0700

    Implement divide() with passing tests

--- src/recursion.js
+++ src/recursion.js
@@ -305,6 +305,41 @@ if (x > 0) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return 0;
+  }
+  if (y === 1) {
+    return x;
+  }
+  var quotient = 0;
+  if (x > 0 && y > 0) {
+    while ((x - y) > 0) {
+      quotient++;
+      x -= y;
+    }
+    return divide(quotient, 1);
+  } else if (x > 0 && y < 0) {
+    while ((x + y) > 0) {
+      quotient--;
+      x += y;
+    }
+    return divide(quotient, 1);
+  } else if (x < 0 && y > 0) {
+    while ((x + y) < 0) {
+      quotient--;
+      x += y;
+    }
+    return divide(quotient, 1);
+  } else if (x < 0 && y < 0) {
+    while ((x - y) < 0) {
+      quotient++;
+      x -= y;
+    }
+    return divide(quotient, 1);
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
